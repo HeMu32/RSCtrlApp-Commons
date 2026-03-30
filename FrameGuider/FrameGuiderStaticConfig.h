ab69cd7888690b5ac4f57b251e9ae0ca40e679d4
HeMu <hemu_32@outlook.com>
2026-03-30 12:54:01 +0800

    Add FrameGuiderStaticConfig

---
+++ FrameGuider/FrameGuiderStaticConfig.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+
+namespace FrameGuiderStaticConfig
+{
+inline constexpr const char* kDefaultTrackerRuntimeDllPath = "./tracker/rsca_trk_runtime.dll";
+inline constexpr const char* kDefaultTrackerModelPath480x288 = "./models/yolo26n-pose-ort-w480-h288.onnx";
+inline constexpr std::int32_t kDefaultTrackerInputFpsLimit = 12;
+}
