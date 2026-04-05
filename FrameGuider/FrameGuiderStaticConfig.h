6b0e82b791ee0c408cb7a7d500c9e3abda50cfc6
HeMu <hemu_32@outlook.com>
2026-04-05 18:06:52 +0800

    Minor

--- FrameGuider/FrameGuiderStaticConfig.h
+++ FrameGuider/FrameGuiderStaticConfig.h
@@ -6,5 +6,5 @@ namespace FrameGuiderStaticConfig
 {
 inline constexpr const char* kDefaultTrackerRuntimeDllPath = "./tracker/rsca_trk_runtime.dll";
 inline constexpr const char* kDefaultTrackerModelPath480x288 = "./models/yolo26n-pose-ort-w480-h288.onnx";
-inline constexpr std::int32_t kDefaultTrackerInputFpsLimit = 12;
+inline constexpr std::int32_t kDefaultTrackerInputFpsLimit = 15;
 }
